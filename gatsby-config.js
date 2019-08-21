a78f70e75da6488562128a1080fc41b8de4217b6
Stefan <stefan@Stefans-Mac-mini.local>
2019-08-21 10:18:13 +0200

    changing description meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Crash course`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `This is a sample website for the Gatsby crash course`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/components/seo.js
+++ src/components/seo.js
@@ -26,7 +26,7 @@ function SEO({ description, lang, meta, title }) {
   )
 
   const metaDescription = description || site.siteMetadata.description
-
+      console.log(metaDescription, "desct")
   return (
     <Helmet
       htmlAttributes={{
