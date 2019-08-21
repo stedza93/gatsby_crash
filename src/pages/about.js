658cee50c749f12c0dc9004546de1d29f4f395b4
Stefan <stefan@Stefans-Mac-mini.local>
2019-08-21 13:17:04 +0200

    added about us page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+const AboutPage = () => {
+  return (
+    <Layout>
+    <div>
+      <h1>About us</h1>
+      <p>Sample text </p>
+    </div>
+  </Layout>
+ 
+  )
+}
+export default AboutPage
